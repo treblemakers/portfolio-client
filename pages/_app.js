8f1b9030397c5b25ca65893863ad07d35f4f16b9
Merculies <merculies@outlook.com>
2020-12-03 08:00:18 +0700

    icon

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,16 @@
-import '../assets/styles/globals.scss';
-import fs from 'fs'
+import "../assets/styles/globals.scss";
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Apisit Promyos</title>
+        <link rel="icon" href="https://res.cloudinary.com/djnasfo5s/image/upload/q_auto,f_auto,o_80/v1605244950/Portfolio/drop4_phwtnh.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
